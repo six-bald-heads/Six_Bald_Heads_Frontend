d1a3357fc4eba80ae2c98cc566715fa4d273c634
nnfeb.18 <jchoi4570@naver.com>
2023-09-20 13:56:30 +0900

    [Feat] add  file and folder creation functionality
    파일 및 폴더 생성 기능 구현

--- src/components/FileDirectory.tsx
+++ src/components/FileDirectory.tsx
@@ -1,45 +1,45 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import { Tree } from 'antd';
-import { FolderOutlined, FileOutlined } from '@ant-design/icons';
-
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Tree } from "antd";
+import { FolderOutlined, FileOutlined } from "@ant-design/icons";
 
 const FileDirectory: React.FC = () => {
+  type Item = {
+    type: "folder" | "file";
+    title: string;
+  };
 
-    type Item = {
-        type: 'folder' | 'file'
-        title : string
-    }
+  const [items, setItems] = useState<Item[]>([]);
 
+  const onRightClick = (itemType) => {
+    const newItem = {
+      type: itemType,
+      title: itemType === "folder" ? "  새 폴더" : "  새 파일",
+    };
+    setItems([...items, newItem]);
+  };
 
-    const [items, setItems] = useState<Item[]>([])
-
-    const onRightClick = (itemType) => {
-        const newItem = {
-            type: itemType,
-            title : itemType === 'folder' ? '새 폴더' : '새 파일'
-        };
-        setItems([...items, newItem]);
-    }
-
-
-    return (
-        <DirectoryContainer>
-            {items.map((item, index)=> (
-                <div key={index}>
-                    {item.type === 'folder' ?  < FolderOutlined /> : < FileOutlined /> }
-                    {item.title}
-                </div>
-            ))}
-            <AddButton onClick={() => onRightClick('folder')}>
-                <FolderOutlined /> +
-            </AddButton>
-            <AddButton onClick={() => onRightClick('file')}>
-                <FileOutlined /> +
-            </AddButton>
-        </DirectoryContainer>
-    );
-}
+  return (
+    <DirectoryContainer>
+      <ButtonContainer>
+        <AddButton onClick={() => onRightClick("folder")}>
+          <FolderOutlined /> +
+        </AddButton>
+        <AddButton onClick={() => onRightClick("file")}>
+          <FileOutlined /> +
+        </AddButton>
+      </ButtonContainer>
+      <ItemsContainer>
+        {items.map((item, index) => (
+          <ItemStyle key={index}>
+            {item.type === "folder" ? <FolderOutlined /> : <FileOutlined />}
+            {item.title}
+          </ItemStyle>
+        ))}
+      </ItemsContainer>
+    </DirectoryContainer>
+  );
+};
 
 const DirectoryContainer = styled.div`
   height: 100%;
@@ -48,29 +48,47 @@ const DirectoryContainer = styled.div`
   box-sizing: border-box;
   padding: 5px;
   padding-left: 15px;
-  padding-right: 20px; 
-  color: #CED0D9;
-  display: flex;  // 여기를 flex로 바꿔
-  justify-content: flex-end; // 수평으로 중앙에 배치
-  align-items: flex-start;     // 수직으로 중앙에 배치
+  padding-right: 30px;
+  color: #ced0d9;
+  display: flex;
+  flex-direction: column;
 `;
 
+const ItemsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ItemStyle = styled.div`
+  font-size: 15px; // 폰트 크기 조절
+  padding: 3px; // 패딩 조절
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  width: 100%;
+  border-bottom: 1px solid #ced0d9;
+  padding-botton: 15px;
+`;
 
 const AddButton = styled.button`
-  background-color: #6D9AE3;  
+  background-color: #6d9ae3;
   border: none;
   color: white;
   text-align: center;
-  display: inline-flex;  
+  display: inline-flex;
   align-items: center;
-  font-size: 8px;  
+  font-size: 10px;
   margin: 0.25rem 0.25rem;
   padding: 5px 10px;
   transition-duration: 0.4s;
   cursor: pointer;
-  
+
   &:hover {
-    background-color: #6486bd;  
+    background-color: #6486bd;
   }
 `;
 
