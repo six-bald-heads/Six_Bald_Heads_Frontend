b5f0292113dadc1786c12a08747919ce62c90bd0
GHeeJeon <jhjeon_99@naver.com>
2023-09-25 02:10:00 +0900

    [Feat] add save request
    sample!

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import CodeMirror, {EditorView, keymap} from '@uiw/react-codemirror';
 import {indentWithTab} from "@codemirror/commands"
@@ -16,6 +16,32 @@ const CodeEditor: React.FC = () => {
         setFontSize(Number(event.target.value));
     };
 
+    const handleSave = async () => {
+        const requestData = {
+            path: "your-path-here",
+            fileName: "your-file-name-here",
+            content: value
+        };
+
+        try {
+            const response = await fetch('/api/v1/editor', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestData),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    };
 
     return (
         <EditorContainer>
@@ -27,9 +53,11 @@ const CodeEditor: React.FC = () => {
                     <option value="18">18px</option>
                     <option value="20">20px</option>
                 </FontSizeSelector>
+                <SaveButton onClick={handleSave}>Save</SaveButton>
             </TopBar>
             <EditorWrapper fontSize={fontSize}>
                 <CodeMirror
+                    className="cm-outer-container"
                     value={value}
                     height="100%"
                     width="100%"
@@ -58,10 +86,28 @@ const EditorContainer = styled.div`
 
 const TopBar = styled.div`
   margin-bottom: 20px;
+  display: flex;
+  align-items: center;
 `;
 
 const FontSizeSelector = styled.select`
-    padding: 5px;
+  padding: 5px;
+  // background-color: #6D9AE3;
+  // color: white;
+`;
+
+const SaveButton = styled.button`
+  padding: 5px 10px;
+  margin-left: 10px;
+  background-color: #6D9AE3;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #CED0D9;
+  }
 `;
 
 const EditorWrapper = styled.div<{ fontSize: number }>`
@@ -69,9 +115,27 @@ const EditorWrapper = styled.div<{ fontSize: number }>`
   width: 100%;
   box-sizing: border-box;
   overflow: scroll;
-  
+
   .cm-outer-container {
     font-size: ${props => props.fontSize}px;
+
+    /* CodeMirror 컴포넌트의 스크롤바 스타일 */
+    ::-webkit-scrollbar {
+      width: 12px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: transparent;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: #CED0D9;
+      border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+      background-color: #b0b0b0;
+    }
   }
 `;
 
