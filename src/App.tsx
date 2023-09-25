dc1e7160a509c4baa1c02e13641b3771e7af991f
GHeeJeon <jhjeon_99@naver.com>
2023-09-26 03:14:38 +0900

    [Feat] create login page
    반응형 레이아웃 적용, cros 우회를 위한 프록시 적용, axios 설치
    요청이 유효한지 검사 필요, 소셜로그인을 고려한 레이아웃 수정 필요

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import MainPage from './pages/MainPage.tsx';
+import LoginPage from "./pages/LoginPage";
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
-            <Route path="/" element={<MainPage />} />
+                <Route path="/" element={<LoginPage/>}/>
+                <Route path="/Main" element={<MainPage/>}></Route>
+
             </Routes>
         </Router>
     );
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,191 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import axios, {AxiosError} from 'axios';
+
+const LoginPage: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        const url = '/api/v1/auth/login';
+        const data = {
+            email,
+            password
+        };
+
+        try {
+            const response = await axios.post(url, data);
+            console.log('Login successful:', response.data);
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const axiosError = error as AxiosError;
+                console.error('Login failed:', axiosError.response ? axiosError.response.data : axiosError.message);
+            } else {
+                console.error('An unknown error occurred:', error);
+            }
+        }
+    };
+
+    const handleSignup = () => {
+        // 회원가입 페이지로 이동 또는 회원가입 로직
+        console.log('Navigating to signup page');
+    };
+
+    return (
+        <LoginContainer>
+            <LoginWrapper>
+                <Title>Six_bald_heads IDE</Title>
+                <Input
+                    type="email"
+                    placeholder="이메일"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                    type="password"
+                    placeholder="비밀번호"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button onClick={handleLogin}>로그인</Button>
+                <Button onClick={handleSignup}>회원가입</Button>
+            </LoginWrapper>
+        </LoginContainer>
+    );
+};
+
+
+const LoginContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #141617;
+`;
+
+const LoginWrapper = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #141617;
+  gap: 20px;
+
+  @media (max-width: 2000px) {
+    width: 55%;
+  }
+
+  @media (max-width: 1700px) {
+    width: 60%;
+  }
+
+  @media (max-width: 1400px) {
+    width: 65%;
+  }
+
+  @media (max-width: 1080px) {
+    width: 70%;
+  }
+
+  @media (max-width: 670px) {
+    width: 80%;
+  }
+`;
+
+
+const Title = styled.h1`
+  color: #CED0D9;
+  font-size: 72px;
+
+  @media (max-width: 2000px) {
+    font-size: 64px;
+  }
+
+  @media (max-width: 1700px) {
+    font-size: 56px;
+  }
+
+  @media (max-width: 1400px) {
+    font-size: 48px;
+  }
+
+  @media (max-width: 1080px) {
+    font-size: 40px;
+  }
+
+  @media (max-width: 670px) {
+    font-size: 36px;
+  }
+`;
+
+
+const Input = styled.input`
+  width: 50%;
+  padding: 10px;
+  box-shadow: 0 0 0 3px #CED0D9;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  font-size: 30px;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 3px #6D9AE3;
+  }
+
+  @media (max-width: 2000px) {
+    font-size: 25px;
+    width: 45%;
+  }
+
+  @media (max-width: 1700px) {
+    font-size: 20px;
+    width: 40%;
+  }
+
+  @media (max-width: 1400px) {
+    font-size: 15px;
+    width: 40%;
+  }
+
+  @media (max-width: 1080px) {
+    font-size: 15px;
+    width: 40%;
+  }
+
+  @media (max-width: 670px) {
+    font-size: 15px;
+    width: 50%;
+  }
+`;
+
+const Button = styled.button`
+  width: 15%;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #CED0D9;
+  color: white;
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    background-color: #6D9AE3;
+  }
+
+  @media (max-width: 1080px) {
+    width: 30%;
+  }
+
+  @media (max-width: 670px) {
+    width: 50%;
+  }
+`;
+
+export default LoginPage;
