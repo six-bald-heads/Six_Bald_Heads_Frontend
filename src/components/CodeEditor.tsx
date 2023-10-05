d60d4ace2c0a4c3c738c2987e6eef864ce41e4a7
GHeeJeon <jhjeon_99@naver.com>
2023-10-06 03:49:38 +0900

    [Feat] add profile modal, 로그아웃 추가

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,85 +1,95 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
-import CodeMirror, { EditorView, keymap } from "@uiw/react-codemirror";
-import { indentWithTab } from "@codemirror/commands";
-import { javascript } from "@codemirror/lang-javascript";
+import CodeMirror, {EditorView, keymap} from "@uiw/react-codemirror";
+import {indentWithTab} from "@codemirror/commands";
+import {javascript} from "@codemirror/lang-javascript";
+import ProfileModal from "./ProfileModal";
 
 const CodeEditor: React.FC = () => {
-  const [value, setValue] = useState("console.log('hello world!');");
-  const [fontSize, setFontSize] = useState(14);
+    const [value, setValue] = useState("console.log('hello world!');");
+    const [fontSize, setFontSize] = useState(14);
 
-  const onChange = (val: string) => {
-    setValue(val);
-  };
-
-  const handleFontSizeChange = (
-    event: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    setFontSize(Number(event.target.value));
-  };
-
-  const handleSave = async () => {
-    const requestData = {
-      path: "/src/bald",
-      fileName: "bald.js",
-      content: value,
+    const onChange = (val: string) => {
+        setValue(val);
     };
 
-    try {
-      const response = await fetch(
-        "http://ec2-3-34-131-210.ap-northeast-2.compute.amazonaws.com:8080/api/v1/editor",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(requestData),
+    const handleFontSizeChange = (
+        event: React.ChangeEvent<HTMLSelectElement>
+    ) => {
+        setFontSize(Number(event.target.value));
+    };
+
+    const handleSave = async () => {
+        const requestData = {
+            path: "/src/bald",
+            fileName: "bald.js",
+            content: value,
+        };
+
+        try {
+            const response = await fetch(
+                "http://ec2-3-34-131-210.ap-northeast-2.compute.amazonaws.com:8080/api/v1/editor",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(requestData),
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok " + response.statusText);
+            }
+
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error(
+                "There has been a problem with your fetch operation:",
+                error
+            );
         }
-      );
+    };
 
-      if (!response.ok) {
-        throw new Error("Network response was not ok " + response.statusText);
-      }
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(
-        "There has been a problem with your fetch operation:",
-        error
-      );
-    }
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
   };
 
-  return (
-    <EditorContainer>
-      <TopBar>
-        <FontSizeSelector onChange={handleFontSizeChange}>
-          <option value="12">12px</option>
-          <option value="14">14px</option>
-          <option value="16">16px</option>
-          <option value="18">18px</option>
-          <option value="20">20px</option>
-        </FontSizeSelector>
-        <SaveButton onClick={handleSave}>저장</SaveButton>
-      </TopBar>
-      <EditorWrapper fontSize={fontSize}>
-        <CodeMirror
-          className="cm-outer-container"
-          value={value}
-          height="100%"
-          width="100%"
-          extensions={[
-            javascript({ jsx: true }),
-            EditorView.lineWrapping,
-            keymap.of([indentWithTab]),
-          ]}
-          theme={"dark"}
-          onChange={onChange}
-        />
-      </EditorWrapper>
-    </EditorContainer>
-  );
+    return (
+        <EditorContainer>
+            <TopBar>
+                <FontSizeSelector onChange={handleFontSizeChange}>
+                    <option value="12">12px</option>
+                    <option value="14">14px</option>
+                    <option value="16">16px</option>
+                    <option value="18">18px</option>
+                    <option value="20">20px</option>
+                </FontSizeSelector>
+                <SaveButton onClick={handleSave}>저장</SaveButton>
+                <Button onClick={handleOpenModal}>프로필</Button>
+                {isModalOpen && <ProfileModal setIsModalOpen={setIsModalOpen} />}
+
+            </TopBar>
+            <EditorWrapper fontSize={fontSize}>
+                <CodeMirror
+                    className="cm-outer-container"
+                    value={value}
+                    height="100%"
+                    width="100%"
+                    extensions={[
+                        javascript({jsx: true}),
+                        EditorView.lineWrapping,
+                        keymap.of([indentWithTab]),
+                    ]}
+                    theme={"dark"}
+                    onChange={onChange}
+                />
+            </EditorWrapper>
+        </EditorContainer>
+    );
 };
 
 const EditorContainer = styled.div`
@@ -100,8 +110,10 @@ const TopBar = styled.div`
 
 const FontSizeSelector = styled.select`
   padding: 5px;
-  // background-color: #6D9AE3;
-  // color: white;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 const SaveButton = styled.button`
@@ -112,11 +124,37 @@ const SaveButton = styled.button`
   border: none;
   border-radius: 5px;
   cursor: pointer;
+  white-space: nowrap;
 
   &:hover {
     background-color: #ced0d9;
   }
+
+  &:focus {
+    outline: none;
+  }
 `;
+
+const Button = styled.button`
+  padding: 5px 10px;
+  margin-left: 10px;
+  background-color: #6d9ae3;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  white-space: nowrap;
+
+  &:hover {
+    background-color: #ced0d9;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+
 
 const EditorWrapper = styled.div<{ fontSize: number }>`
   height: calc(100% - 50px);
---
+++ src/components/ProfileModal.tsx
@@ -0,0 +1,236 @@
+import React, {useState} from 'react';
+import { useAuth } from "../hooks/useAuth.ts"
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import {CloseOutlined, EditOutlined} from "@ant-design/icons";
+import axios, {AxiosError} from "axios";
+
+interface ProfileModalProps {
+    setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const ProfileModal: React.FC<ProfileModalProps> = ({ setIsModalOpen }) => {
+    const [nickname, setNickname] = useState('');
+    const [password, setPassword] = useState('');
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+
+    const handleClose = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleNicknameChange = () => {
+        // 닉네임 변경 로직
+    };
+
+    const handlePasswordChange = () => {
+        // 비밀번호 변경 로직
+    };
+
+    const handleDeleteAccount = () => {
+        // 회원탈퇴 로직
+    };
+
+    const handleLogout = async () => {
+        const url = 'http://ec2-3-34-131-210.ap-northeast-2.compute.amazonaws.com:8080/api/v1/auth/logout';
+
+        try {
+            const response = await axios.post(url);
+
+            if (response.status === 200) {
+                console.log('로그아웃 성공! : ', response.data);
+                logout();
+                navigate('/login');
+            } else {
+                console.error('예상치 못한 문제가 발생했어요! : ', response.status, response.data);
+            }
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const axiosError = error as AxiosError;
+                if (axiosError.response) {
+                    console.error('네트워크 혹은 서버 에러입니다. : ', axiosError.response.data);
+                } else {
+                    console.error('네트워크 혹은 서버 에러입니다. : ', axiosError.message);
+                }
+            } else {
+                console.error('예상치 못한 문제가 발생했어요! : ', error);
+            }
+        }
+    };
+
+    return (
+        <ModalWrapper>
+            <ModalContainer>
+                <ModalHeader>
+                    <CloseButton onClick={handleClose}>
+                        <CloseOutlined/>
+                    </CloseButton>
+                </ModalHeader>
+                <ProfileContainer>
+                    <UploadButton>
+                        <EditOutlined/>
+                    </UploadButton>
+                    <ModifyWrapper>
+                        <Input
+                            type="text"
+                            placeholder="닉네임 변경"
+                            value={nickname}
+                            onChange={(e) => setNickname(e.target.value)}
+                        />
+                        <Button onClick={handleNicknameChange}>닉네임 변경</Button>
+                    </ModifyWrapper>
+                    <ModifyWrapper>
+                        <Input
+                            type="password"
+                            placeholder="비밀번호 변경"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Button onClick={handlePasswordChange}>비밀번호 변경</Button>
+                    </ModifyWrapper>
+                    <Button onClick={handleLogout}>로그아웃</Button>
+                    <WithdrawButton onClick={handleDeleteAccount}>회원탈퇴</WithdrawButton>
+                </ProfileContainer>
+
+            </ModalContainer>
+        </ModalWrapper>
+    );
+};
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+`;
+
+const ModalContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+  height: 80%;
+  background-color: #CED0D9;
+  border-radius: 10px;
+  z-index: 1000;
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const ModalHeader = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 20px;
+`;
+
+const CloseButton = styled.button`
+  background: none;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  font-size: 20px;
+  cursor: pointer;
+  color: #141617;
+  left: 20px;
+
+  &:active {
+    outline: none;
+    border: none;
+  }
+
+  &:hover {
+    color: #6d9ae3;
+  }
+`;
+
+const UploadButton = styled.button`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  outline: none;
+  cursor: pointer;
+
+  &:active {
+    outline: none;
+    border: none;
+  }
+`;
+
+const ModifyWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Input = styled.input`
+  width: clamp(25px, 50vw, 200px);
+  padding: 10px;
+  box-shadow: 0 0 0 3px #CED0D9;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 3px #6D9AE3;
+  }
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  background-color: #303336;
+  color: white;
+  cursor: pointer;
+
+  &:active {
+    outline: none;
+    border: none;
+  }
+
+  &:hover {
+    background-color: #6d9ae3;
+  }
+`;
+
+const WithdrawButton = styled.button`
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  color: #646060;
+
+  &:active {
+    outline: none;
+    border: none;
+  }
+
+  &:hover {
+    color: #6d9ae3;
+  }
+`;
+
+export default ProfileModal;
